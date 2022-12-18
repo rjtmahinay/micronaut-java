3bf3127aa8218e2ea2aeb69e82c68f40a2ea8246
Reynaldo John Tristan Mahinay <Reynaldo.Mahinay@ibm.com>
2022-12-18 08:00:32 +0800

    feat: initial exception handling

---
+++ src/main/java/com/rjtmahinay/exception/EmployeeException.java
@@ -0,0 +1,11 @@
+package com.rjtmahinay.exception;
+
+public class EmployeeException extends RuntimeException {
+    public EmployeeException(String message) {
+        super(message);
+    }
+
+    public EmployeeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
